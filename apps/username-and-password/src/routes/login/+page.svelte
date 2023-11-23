b0d75761ad09627b4e48c734da6fcd7ec4954e63
Chris Ian Fiel <ccfiel@gmail.com>
2023-11-23 10:26:52 +0800

    Update login form to use SignInStore

--- apps/username-and-password/src/routes/login/+page.svelte
+++ apps/username-and-password/src/routes/login/+page.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { SignInStore } from "$houdini";
 
-	import type { ActionData } from './$types';
+	const signin = new SignInStore()
 
-	export let form: ActionData;
+	async function onSignIn(event: Event) {
+		event.preventDefault();
+		const target = event.target as typeof event.target & {
+			username: { value: string };
+			password: { value: string };
+		};
+		const username = target.username.value;
+		const password = target.password.value;
+		console.log('onSignUp', username, password);
+		const res = await signin.mutate({ username, password });
+		console.log('res', res);
+	}
 </script>
 
 <h1>Sign in</h1>
-<form method="post" use:enhance>
+<form method="post" on:submit={onSignIn}>
 	<label for="username">Username</label>
 	<input name="username" id="username" /><br />
 	<label for="password">Password</label>
 	<input type="password" name="password" id="password" /><br />
 	<input type="submit" />
 </form>
-{#if form?.message}
+<!-- {#if form?.message}
 	<p class="error">{form.message}</p>
-{/if}
+{/if} -->
 <a href="/signup">Create an account</a>
