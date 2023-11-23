aa3be08c0a10cd0d5fcd5880614fcdd9ad132168
Chris Ian Fiel <ccfiel@gmail.com>
2023-11-23 11:41:16 +0800

    Commented out unused code and deleted unnecessary files

--- apps/username-and-password/src/routes/signup/+page.svelte
+++ apps/username-and-password/src/routes/signup/+page.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
-    import { SignUpStore } from "$houdini";
+	import { SignUpStore } from '$houdini';
 
-    const signup = new SignUpStore()
-    let errorMessage: string = '';
+	const signup = new SignUpStore();
+	let errorMessage: string = '';
 
-    async function onSignUp(event: Event) {
-        event.preventDefault();
-        const target = event.target as typeof event.target & {
-            username: { value: string };
-            password: { value: string };
-        };
-        const username = target.username.value;
-        const password = target.password.value;
-		console.log('onSignUp', username, password)
-        const res = await signup.mutate({ username, password });
-        if (res.errors?.length ?? 0 > 0) {
-            if (res.errors && res.errors.length > 0 && res.errors[0].message === 'AUTH_DUPLICATE_KEY_ID') {
-                errorMessage = 'Username already exists'
-            }
-        }
-    }
+	async function onSignUp(event: Event) {
+		event.preventDefault();
+		const target = event.target as typeof event.target & {
+			username: { value: string };
+			password: { value: string };
+		};
+		const username = target.username.value;
+		const password = target.password.value;
+		console.log('onSignUp', username, password);
+		const res = await signup.mutate({ username, password });
+		if (res.errors?.length ?? 0 > 0) {
+			if (
+				res.errors &&
+				res.errors.length > 0 &&
+				res.errors[0].message === 'AUTH_DUPLICATE_KEY_ID'
+			) {
+				errorMessage = 'Username already exists';
+			}
+		}
+	}
 </script>
 
 <h1>Sign up</h1>
