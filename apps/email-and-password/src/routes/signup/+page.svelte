1e4c776320f29939394e9a074e5315334a469455
Chris Ian Fiel <ccfiel@gmail.com>
2023-11-24 14:47:00 +0800

    Add Houdini dependency and import in Svelte config

--- apps/email-and-password/src/routes/signup/+page.svelte
+++ apps/email-and-password/src/routes/signup/+page.svelte
@@ -1,20 +1,48 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { SignUpStore } from '$houdini';
+	import { user } from '$lib/store';
+	import { goto } from '$app/navigation';
 
-	import type { ActionData } from './$types';
+	const signup = new SignUpStore();
+	let errorMessage: string = '';
 
-	export let form: ActionData;
+	async function onSignUp(event: Event) {
+		event.preventDefault();
+		const target = event.target as typeof event.target & {
+			username: { value: string };
+			password: { value: string };
+		};
+		const username = target.username.value;
+		const password = target.password.value;
+
+		const res = await signup.mutate({ username, password });
+		if (res.errors?.length ?? 0 > 0) {
+			if (res.errors && res.errors.length > 0) {
+				errorMessage = res.errors[0].message;
+			}
+			if (errorMessage == 'AUTH_DUPLICATE_KEY_ID') {
+				errorMessage = 'Username already exists';
+			}
+		} else {
+			user.set({
+				id: res.data?.signup.user.userId ?? '',
+				sessionId: res.data?.signup.sessionId ?? '',
+				name: username
+			});
+			goto('/');
+		}
+	}
 </script>
 
 <h1>Sign up</h1>
-<form method="post" use:enhance>
-	<label for="email">Email</label>
-	<input name="email" id="email" /><br />
+<form method="post" on:submit={onSignUp}>
+	<label for="username">Username</label>
+	<input name="username" id="username" /><br />
 	<label for="password">Password</label>
 	<input type="password" name="password" id="password" /><br />
 	<input type="submit" />
 </form>
-{#if form?.message}
-	<p class="error">{form.message}</p>
+{#if errorMessage}
+	<p class="error">{errorMessage}</p>
 {/if}
 <a href="/login">Sign in</a>
