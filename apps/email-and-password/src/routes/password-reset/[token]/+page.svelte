c079d1933d87183d2e54ebb4415e8e091c7c6f2d
Chris Ian Fiel <ccfiel@gmail.com>
2023-11-26 21:59:03 +0800

    Fix password reset page and update database

--- apps/email-and-password/src/routes/password-reset/[token]/+page.svelte
+++ apps/email-and-password/src/routes/password-reset/[token]/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { ChangePasswordStore } from '$houdini'
-	import { page } from "$app/stores";
+	import { ChangePasswordStore } from '$houdini';
+	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 
 	let errorMessage: string = '';
 
 	async function onChangePassword(event: Event) {
-		event.preventDefault()
+		event.preventDefault();
 		const target = event.target as typeof event.target & {
-			password: { value: string }
-		}
-		const password = target.password.value
+			password: { value: string };
+		};
+		const password = target.password.value;
 
-		const store = new ChangePasswordStore()
-		const res = await store.mutate({ password, token: $page.params.token })
-		console.log('res')
-		console.log(res)
+		const store = new ChangePasswordStore();
+		const res = await store.mutate({ password, token: $page.params.token });
+		console.log('res');
+		console.log(res);
 		if (res.errors?.length ?? 0 > 0) {
 			if (res.errors && res.errors.length > 0) {
-				errorMessage = res.errors[0].message
+				errorMessage = res.errors[0].message;
 			}
 			if (errorMessage == 'AUTH_INVALID_KEY_ID' || errorMessage == 'AUTH_INVALID_PASSWORD') {
-				errorMessage = 'Username or password is incorrect'
+				errorMessage = 'Username or password is incorrect';
 			}
-		} 
-		// goto('/');
+		} else {
+			goto('/');
+		}
 	}
-
 </script>
 
 <h1>Reset password</h1>
